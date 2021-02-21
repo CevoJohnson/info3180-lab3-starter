d0b88c792ef432ef218a2df02ebcd11b2ec64f3c
Kevin Johnson <kevin.johnson_12@hotmail.com>
2021-02-20 20:04:41 -0500

    Completed Assignment

--- app/config.py
+++ app/config.py
@@ -4,7 +4,7 @@ class Config(object):
     """Base Config Object"""
     DEBUG = False
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'Som3$ec5etK*y'
-    MAIL_SERVER = os.environ.get('SECRET_KEY') or 'localhost'
+    MAIL_SERVER = os.environ.get('MAIL_SERVER') or 'localhost'
     MAIL_PORT = os.environ.get("MAIL_PORT") or '25'
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
